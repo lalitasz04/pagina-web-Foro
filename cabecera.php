0f3e8ea0c347f22991a7019a50c8d00ca1654c05
MileColmenares <132697364+MileColmenares@users.noreply.github.com>
2023-05-30 14:57:43 -0300

    Add files via upload

---
+++ cabecera.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centro de estudiantes</title>
+    <link rel="shortcut icon" href="./imagenes/logoipet-favicon.jpg">
+    <!--BOOSTRAP--> 
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+   <!--CSS-->
+   <link rel="stylesheet" href="css/style1.css">
+   <!--JAVA-->
+       <script src="js/style1.js"></script>
+</head>
+<body>
+    <header>
+        <a href="a" div class="logo">
+            <img src="imagenes/logoCentro.jpeg" alt="logo centro">
+        </div>
+        <nav>
+            <a href="" class="nav-link"><button type="button" class="btn btn-outline-success">Foro</button></a>
+            <a href="" class="nav-link"><button type="button" class="btn btn-outline-danger">Personal</button></a>
+            <a href="" class="nav-link"><button type="button" class="btn btn-outline-info">Comentarios</button></a>
+            <a href="" class="nav-link"><img src="imagenes/usuario.png" alt="logo centro" height="50px"></a>
+        </nav>
+    </header>
+
+
+
+    
+    
+    
+</body>
+</html>
