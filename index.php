203a8ca7d109183d0e22dc34a28226950bf817fa
MileColmenares <132697364+MileColmenares@users.noreply.github.com>
2023-06-05 13:52:53 -0300

    Add files via upload
    cabecera + footer

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="shortcut icon" href="./imagenes/logoipet-favicon.jpg">
+    <!--BOOSTRAP--> 
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+   <!--CSS-->
+   <link rel="stylesheet" href="css/style2.css">
+   <!--JAVA-->
+       <script src="js/style2.js"></script>
+</head>
+<body>
+<!--CABECERA-->
+    <?php
+    include("cabecera.php");
+    ?>
+
+
+<!--LINK DIRECTO A WHATSAPP-->
+    <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+    <div class="elfsight-app-9a20d9c0-75b9-4a5f-a047-e1a4ae679bfe"></div>
+<!--FOOTER-->
+    <?php
+    include("footer.php");
+    ?>
+</body>
+</html>
