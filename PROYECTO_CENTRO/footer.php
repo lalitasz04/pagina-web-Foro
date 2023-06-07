9252b9d722091d51b9304498b6542d191c0d22e7
MileColmenares <132697364+MileColmenares@users.noreply.github.com>
2023-06-07 18:03:20 -0300

    index.phpFinal-milena

---
+++ PROYECTO_CENTRO/footer.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="viaje.css">
+    <script src="https://kit.fontawesome.com/5687f4a15a.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Viaje</title>
+   
+    <!--CSS-->
+   <link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+    <div class="counter">
+        <div class="image">
+            <img src="foto1 .jpeg" alt="">
+            <i class="text"><p>Fácil</p></i>
+        </div>
+        <div class="image">
+            <img src="foto2.jpg" alt="">
+            <i class="text"><p>Cómodo</p></i>
+        </div>
+        <div class="image">
+            <img src="foto3.jpeg" alt="">
+            <i class="text"><p>Explorable</p></i>
+        </div>
+    </div>
+     <footer>
+        <div class="footer-content">
+            <h3>Centro de Estudiantes</h3>
+            <p>Creado por y para alumnos, con la intenciòn de crear un mejor ambiente en la instituciòn, todos son bienvenidos a participar y tienen el derecho de saber.</p>
+            <div>
+                <a href="./Estatuto_centro.docx" class="nav-link"><button type="button" class="btn btn-outline-light">Descargar estatuto</button></a>
+            </div>
+            <div class="iconos">
+                <container class="ig"><a href="#"><i class="fa-brands fa-instagram"></i></a></container>
+            </div>
+        </div>
+        <div>
+        
+        <div class="footer-bottom">
+            <p>copyright &copy;2023 By: The Chiappori Team</p>
+        </div>
+     </footer>
+</body>
+</html>
---
+++ PROYECTO_CENTRO/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="shortcut icon" href="./imagenes/logoipet-favicon.jpg">
+   <!--CSS-->
+   <link rel="stylesheet" href="css/style2.css">
+   <!--JAVA-->
+       <script src="js/style2.js"></script>
+</head>
+<body>
+<!--CABECERA-->
+    <?php
+    include("cabecera.php");
+    ?>
+
+
+<!--LINK DIRECTO A WHATSAPP-->
+    <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+    <div class="elfsight-app-9a20d9c0-75b9-4a5f-a047-e1a4ae679bfe"></div>
+<!--FOOTER-->
+    <?php
+    include("footer.php");
+    ?>
+</body>
+</html>
